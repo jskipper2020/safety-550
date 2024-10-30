fe57a74a9d3384642225b438a0f7f55388cf91f6
jskipper2020 <79345377+jskipper2020@users.noreply.github.com>
2024-10-30 01:26:25 -0500

    got popups to appear but they won't the first time the icon is clicked for some reason

--- scripts/background.js
+++ scripts/background.js
@@ -5,13 +5,13 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.action.onClicked.addListener(() => {
     chrome.storage.local.get("mode", (result) => {
       let url;
-      if (result.mode === "child") {
+      if (result.mode[0] === "child") {
         url = "popup_c.html";
-        chrome.action.setPopup({ url });
+        chrome.action.setPopup({ popup: url });
         chrome.action.openPopup();
-      } else if (result.mode === "adult") {
+      } else if (result.mode[0] === "adult") {
         url = "popup_a.html";
-        chrome.action.setPopup({ url });
+        chrome.action.setPopup({ popup: url });
         chrome.action.openPopup();
       } else {
         url = "welcome.html";
