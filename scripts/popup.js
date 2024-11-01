1061810ef91ec36dcee37ecc1961b5ed84c7d0ed
jskipper2020 <jskipper2020@gmail.com>
2024-11-01 18:14:03 -0500

    it wasn't even running the javascript for 5-star, now it needs to be fixed

--- scripts/popup.js
+++ scripts/popup.js
@@ -19,9 +19,32 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(response => response.text())
             .then(html => {
                 contentDiv.innerHTML = html;
+                stars();
             })
             .catch(error => {
                 console.error('Error loading content:', error);
             });
     });
 });
+
+function stars() {
+    const stars = document.querySelectorAll('.star');
+    const ratingValue = document.getElementById('rating-value');
+    console.log("It reaches here");
+    stars.forEach((star) => {
+        star.addEventListener('click', function () {
+        const rating = this.getAttribute('data-value');
+        console.log(rating);
+        ratingValue.textContent = rating;
+        
+        stars.forEach(s => s.classList.remove('selected'));
+
+        this.classList.add('selected');
+        let previousStar = this.previousElementSibling;
+        while (previousStar) {
+            previousStar.classList.add('selected');
+            previousStar = previousStar.previousElementSibling;
+        }
+        });
+    });
+}
